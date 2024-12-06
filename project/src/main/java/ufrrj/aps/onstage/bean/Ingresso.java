afb3bb82d357436278818d09258f9b440d107466
Garcia <gabrielgarciagc175@gmail.com>
2024-12-06 02:51:16 -0300

    Merge com as classes da Sol

---
+++ project/src/main/java/ufrrj/aps/onstage/bean/Ingresso.java
@@ -0,0 +1,55 @@
+package aps.onstage.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Ingresso")
+public class Ingresso {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String tipo;
+    private Double preco;
+    private int quantidade;
+
+    @Column(name = "data_compra")
+    private String dataCompra;
+
+    public Ingresso(String tipo, Double preco) {
+        this.tipo = tipo;
+        this.preco = preco;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+    public int getQuantidade() {
+        return quantidade;
+    }
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+    public String getDataCompra() {
+        return dataCompra;
+    }
+}

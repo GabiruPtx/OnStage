74a07386548bbe91c546a6652519ecc12510d255
libonatii <analuzlibonati@gmail.com>
2024-12-10 01:09:49 -0300

    Atualização ingressos....

---
+++ project/src/main/webapp/js/selecaoingressos.js
@@ -0,0 +1,19 @@
+
+// Alterna entre os temas (escuro e claro)
+document.getElementById('theme-toggle').addEventListener('click', function () {
+    // Alterna as classes de tema
+    const isDarkTheme = document.body.classList.toggle('black-theme');
+    document.body.classList.toggle('white-theme', !isDarkTheme);
+  
+    // Alterar o ícone do botão conforme o tema
+    let icon = document.querySelector(".theme-toggle .icon");
+    if (isDarkTheme) {
+      icon.innerText = "🌙"; // Lua para tema escuro
+    } else {
+      icon.innerText = "☀️"; // Sol para tema claro
+    }
+  
+    // Atualizar a logo com base no tema
+    const logo = document.querySelector('.logo');
+    logo.src = isDarkTheme ? 'img/OS_Logotipo_White.png' : 'img/OS_Logotipo_Black.png';
+  });
